3d1d5a46cb865b12426bf235650608474f956e7b
Paolo Missagia <login@paolomissagia.com>
2023-07-14 19:14:18 +0100

    basic flask app endpoint call

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
+import os
+from supabase import create_client, Client
 from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
+
+# flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# supabase
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+
+# routes
+@app.route('/<level>/<category>')
+def get_guide_by_level_and_category(level = None, category = None):
+    # query supabase
+    response = supabase.table('guides').select("*").eq('level', level).eq('category', category).execute()
+    # return response
+    return str(response.data)
